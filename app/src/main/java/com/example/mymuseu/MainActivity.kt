0619ca4224b6880bc91fab74a18fea48d14b8dcd
Yusufaky <marcosta71999@gmail.com>
2020-05-13 21:18:02 +0100

    QRCODE

--- app/src/main/java/com/example/mymuseu/MainActivity.kt
+++ app/src/main/java/com/example/mymuseu/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.mymuseu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_q_r_code.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btn_menu)
+        button.setOnClickListener{
+            val intent = Intent(this, QRCode::class.java)
+            startActivity(intent)
+        }
     }
 }
