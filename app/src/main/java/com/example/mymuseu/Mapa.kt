ef11bb93dabe2d02bffa047729768c3a423b734e
saleiro <saleiro94>
2020-05-15 21:46:54 +0100

    mapa

--- app/src/main/java/com/example/mymuseu/Mapa.kt
+++ app/src/main/java/com/example/mymuseu/Mapa.kt
@@ -1,14 +1,77 @@
 package com.example.mymuseu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class Mapa : AppCompatActivity() {
+
+class Mapa : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var googleMap: GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa)
         supportActionBar!!.title= "Mapa"//getString(R.string.contactos)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
-}
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap = map?: return
+        val mapSettings = map?.uiSettings
+        val museutraje = LatLng(41.69293, -8.82855)
+
+        googleMap.addMarker(
+            MarkerOptions().position(museutraje)
+                .title("Museu Traje")
+        )
+
+        enableMyLocation()
+    }
+    private fun isPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            googleMap.isMyLocationEnabled = true
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+    }
+
+
+
+
+
+
+
+
