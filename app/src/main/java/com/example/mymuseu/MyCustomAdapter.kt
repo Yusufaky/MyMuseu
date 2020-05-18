0d9ec2ae2808117f49e261bc555c487828b6f570
saleiro <saleiro94>
2020-05-18 15:00:34 +0100

    opiniao

---
+++ app/src/main/java/com/example/mymuseu/MyCustomAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.mymuseu
+
+class MyCustomAdapter {
+}
---
+++ app/src/main/java/com/example/mymuseu/Novaopiniao.kt
@@ -0,0 +1,12 @@
+package com.example.mymuseu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Novaopiniao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novaopiniao)
+    }
+}
